7fcb2e788b1bd612d05e83917de5e7bc0f678ba1
bearsanity <jsweeney96@hotmail.com>
2026-03-07 18:20:35 -0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+ function App() {
+  return (
+    <div>
+      <h1>test text</h1>
+      <h2>looks like its working</h2>
+      <h6>small text</h6>
+    </div>)
+ }
+
+export default App

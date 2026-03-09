f0635e096f4548e5bc14e45e5330712726ed8d4d
bearsanity <jsweeney96@hotmail.com>
2026-03-09 20:53:43 -0230

    finished Resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,23 @@
  function Resume() {
   return (
     <div>
-        <h1>Resume</h1>
+        <h2>Resume</h2>
+        <h3>Technical Proficiencies</h3>
+        <ul>
+            <li>Javascript</li>
+            <li>Python</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>PostgreSQL</li>
+            <li>MongoDB</li>
+            <li>React</li>
+            <li>Express</li>
+            <li>Node</li>
+            <li>Sequelize</li>
+            <li>Mongoose</li>
+            <li>Git</li>
+        </ul>
+        <a href='/ResumeMarch2026.pdf' download='JamesSweeneyResume'>Download my resume (PDF)</a>
     </div>)
  }
 

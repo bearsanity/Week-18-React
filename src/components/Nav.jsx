bbd14a9dfd15dd7a9838ef5d8a776cd72231a6d3
bearsanity <jsweeney96@hotmail.com>
2026-03-10 01:23:39 -0230

    navbar order change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,9 +4,9 @@ function Nav() {
     return (
         <nav>
             <NavLink to="/">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
             <NavLink to="/resume">Resume</NavLink>
             <NavLink to="/portfolio">Portfolio</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
         </nav>
     );
 }
